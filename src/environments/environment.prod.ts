c5db9c4e04d633b9578dabf1c4ce5ee71d9b0a55
starobar <barborastaronova@gmail.com>
2021-01-25 17:34:04 +0100

    deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyD-mlwmmGwfQrJzUepyfrvrA5FfKaWqeFk",
+    authDomain: "projectcovid-d9ab2.firebaseapp.com",
+    projectId: "projectcovid-d9ab2",
+    storageBucket: "projectcovid-d9ab2.appspot.com",
+    messagingSenderId: "99551099304",
+    appId: "1:99551099304:web:77cdb6e2678b508e3d059d",
+    measurementId: "G-VNQ0993F62"
+  }
 };
